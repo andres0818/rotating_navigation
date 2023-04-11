313e9b300c919b8e20d112d8b7fc61056b2293ba
andres0818 <c.andres.perez.h@gmail.com>
2023-04-11 15:41:40 -0500

    add component information

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Nav from "./components/Nav/Nav";
+import Information from "./components/Information/Information";
 
 function App() {
   const [rotation, setRotation] = useState({ nav: 0, content: 0 });
 
   return (
-    <>
+    <div className="app">
+      <Information rotation={rotation} setRotation={setRotation} />
       <Nav rotation={rotation} setRotation={setRotation} />
-    </>
+    </div>
   );
 }
 
